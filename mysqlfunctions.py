16eec7c17050c3779c54b793f162bb520a226c90
jergk <37027816+jergk@users.noreply.github.com>
2020-07-14 19:14:54 +0200

    commands register and participate

--- mysqlfunctions.py
+++ mysqlfunctions.py
@@ -1,35 +1,55 @@
 import mysql.connector
 import pw
 
+
 def add_userscore_tournament():
     mydb = connectToDB()
     mycursor = mydb.cursor()
 
     mycursor.execute("SELECT COUNT(*) FROM `pingpong`.`tournaments`;")
-    tournamentnumber =str(mycursor.fetchone()[0])
+    tournamentnumber = str(mycursor.fetchone()[0])
 
     mycursor.execute("\
         ALTER TABLE `pingpong`.`userscores` \
         ADD COLUMN t" + tournamentnumber + "team  VARCHAR(45) NULL AFTER `userid`,\
-        ADD COLUMN t" + tournamentnumber + "participated TINYINT NULL AFTER `t" + tournamentnumber + "team`,\
-        ADD COLUMN t" + tournamentnumber + "wins INT ZEROFILL NULL AFTER `t" + tournamentnumber + "participated`,\
+        ADD COLUMN t" + tournamentnumber + "wins INT ZEROFILL NULL AFTER `t" + tournamentnumber + "team`,\
         ADD COLUMN t" + tournamentnumber + "kills INT ZEROFILL NULL AFTER `t" + tournamentnumber + "wins`,\
         ADD COLUMN t" + tournamentnumber + "deaths INT ZEROFILL NULL AFTER `t" + tournamentnumber + "kills`,\
         ADD COLUMN t" + tournamentnumber + "placement INT ZEROFILL NULL AFTER `t" + tournamentnumber + "deaths`,\
-        ADD COLUMN t" + tournamentnumber + "score INT ZEROFILL NULL AFTER `t" + tournamentnumber + "placement`;\
+        ADD COLUMN t" + tournamentnumber + "score INT ZEROFILL NULL AFTER `t" + tournamentnumber + "placement`,\
+        ADD COLUMN t" + tournamentnumber + "participates TINYINT NULL AFTER `t" + tournamentnumber + "score`,\
+        ADD COLUMN t" + tournamentnumber + "disqualified TINYINT NULL AFTER `t" + tournamentnumber + "participates`;\
     ")
     mycursor.close()
     mydb.close()
 
 
-def add_tournament_tournament(date, gamemap, gametype, speed, comment="no comment"):
+def add_tournament_tournament(date, gamemap, gametype, speed, comment):
     mydb = connectToDB()
     mycursor = mydb.cursor()
 
     mycursor.execute(
-        "INSERT INTO `pingpong`.`tournaments` (tdate, tmap, ttype, tcomment, tspeed) "+
+        "INSERT INTO `pingpong`.`tournaments` (tdate, tmap, ttype, tcomment, tspeed) " +
         "VALUES ('" + date + "','" + gamemap + "','" + gametype + "','" + speed + "','" + comment + "');"
     )
+    mycursor.close()
+    mydb.commit()
+    mydb.close()
+
+def participate(dctag):
+    mydb = connectToDB()
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT COUNT(*) FROM `pingpong`.`tournaments`;")
+    number = str(mycursor.fetchone()[0])
+
+    mycursor.execute("Select idusers From `pingpong`.`users` Where discordtag='"+dctag+"'")
+    userid = str(mycursor.fetchone()[0])
+
+    #TODO add time constraints
+
+    mycursor.execute("UPDATE `pingpong`.`userscores` SET t" + number + "participates" +
+                     "=True Where userid= " + userid + ";")
     mycursor.close()
     mydb.commit()
     mydb.close()
@@ -37,6 +57,26 @@ def add_tournament_tournament(date, gamemap, gametype, speed, comment="no commen
 def getScoreboard():
     mydb = connectToDB()
 
+
+def register(battletag, dctag, name, email, nickname):
+    mydb = connectToDB()
+    mycursor = mydb.cursor()
+
+    if (not nickname):
+        nickname = name
+
+    mycursor.execute("INSERT INTO `pingpong`.`users` (battletag, discordtag, name, nickname, email)" +
+                     "VALUES ('" + battletag + "','" + dctag + "','" + name + "','" + nickname + "','" + email+"');")
+
+    mycursor.execute("Select idusers From `pingpong`.`users` Where discordtag = '"+ dctag+"'")
+    userid = str(mycursor.fetchone()[0])
+
+    mycursor.execute("INSERT INTO `pingpong`.`userscores` (userid)" +
+                         "VALUES ('" + userid + "');")
+
+    mycursor.close()
+    mydb.commit()
+    mydb.close()
 
 def connectToDB():
     mydb = mysql.connector.connect(
@@ -46,7 +86,5 @@ def connectToDB():
     )
     return mydb
 
-
-#add_tournament_tournament("14/07/2020","roundboy","1v1","80")
-#add_userscore_tournament()
-
+# add_tournament_tournament("14/07/2020","roundboy","1v1","80")
+# add_userscore_tournament()
--- pw.py
+++ pw.py
@@ -1,7 +1,7 @@
 # passwords and such
 
-bot_token = ""
-db_pw = ""
+bot_token = "NzI1ODU1NDQzNTkwODQwMzYw.Xw2c4Q.Z_PTHqJf2H3g6BGP0lD_sb-UPHc"
+db_pw = "Xcv.Kl9=fg6%ztsdnldf0}"
 
 def get_bot_token():
     return bot_token
